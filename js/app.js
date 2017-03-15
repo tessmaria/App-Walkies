76cf319d7b54f12e5b1841f93e26d3557ceb201a
Teresa Amass <teresa_amass@hotmail.com>
2017-03-15 16:43:16 +0000

    making states

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 angular.module('app', ['ionic'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
+            
             .state('parks', {
                 url: '/',
                 templateUrl: './states/parks.html',
                 controller: 'ParkController',
                 name: 'Parks'
             })
+
+            .state('beaches', {
+                url: '/beaches',
+                templateUrl: './states/beaches.html',
+                controller: 'BeachController',
+                name: 'Beaches'
+            })
+
             .state('map', {
                 url: '/map/:id',
                 templateUrl: './states/map.html',
