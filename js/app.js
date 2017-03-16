aa146eb40c0ee3003a03be57311088c3551ed1fe
Teresa Amass <teresa_amass@hotmail.com>
2017-03-16 12:31:50 +0000

    adding all the states

--- js/app.js
+++ js/app.js
@@ -16,6 +16,27 @@ angular.module('app', ['ionic'])
                 name: 'Beaches'
             })
 
+            .state('mountains', {
+                url: '/mountains',
+                templateUrl: './states/mountains.html',
+                controller: 'BeachController',
+                name: 'Mountains'
+            })
+
+            .state('woods', {
+                url: '/woods',
+                templateUrl: './states/woods.html',
+                controller: 'BeachController',
+                name: 'Woods'
+            })
+
+            .state('cities', {
+                url: '/cities',
+                templateUrl: './states/cities.html',
+                controller: 'BeachController',
+                name: 'Cities'
+            })
+
             .state('map', {
                 url: '/map/:id',
                 templateUrl: './states/map.html',
